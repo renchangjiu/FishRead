92bf3c66c0672ade40577305efda47179c0847a4
su <1359581862@qq.com>
2019-10-12 10:50:30 +0800

    commit

--- log.py
+++ log.py
@@ -1,10 +1,12 @@
+import os
+
 from app_attribute import AppAttribute as app
 
 
 class ReadLog(object):
 
     @staticmethod
-    def get_schedule(book_path) -> int:
+    def read_schedule(book_path) -> int:
         path = app.root + "/read.log"
         file = open(path, "r", encoding="utf=8")
         lines = file.readlines()
@@ -17,13 +19,27 @@ class ReadLog(object):
         return schedule
 
     @staticmethod
-    def set_schedule(book_path: str, schedule: int):
+    def write_schedule(book_path: str, schedule: int):
         path = app.root + "/read.log"
         file = open(path, "a", encoding="utf=8")
         file.write("%s?%d\r" % (book_path, schedule))
         file.close()
 
+    @staticmethod
+    def get_last_book():
+        """读取阅读记录中的最后一条记录的文件路径"""
+        path = app.root + "/read.log"
+        if not os.path.isfile(path):
+            open(path, "w", encoding="utf=8").close()
+        file = open(path, "r", encoding="utf=8")
+        lines = file.readlines()
+        book_path = ""
+        if len(lines) != 0:
+            book_path = lines[-1].split("?")[0]
+        file.close()
+        return book_path
+
 
 if __name__ == "__main__":
     log = ReadLog()
-    print(log.get_schedule("D:/Downloads/仙逆-贴吧精校版.txt"))
+    print(log.read_schedule("D:/Downloads/仙逆-贴吧精校版.txt"))
--- main.py
+++ main.py
@@ -1,105 +1,106 @@
 import os
-import re
 import msvcrt
 import sys
-from typing import TextIO
+import math
+
+import click
 
 from app_attribute import AppAttribute as app
 from log import ReadLog
 
-null = None
 
-book_path = "D:/Downloads/仙逆-贴吧精校版.txt"
+@click.command()
+@click.option("-p", default="", help="文件路径, 若为空, 则读取阅读记录中的最后一条记录")
+@click.option("-r", default=1, help="每页显示的行数, 默认为: 1, 示例: 5")
+@click.option("-s", default=0, help="跳页, 默认为文件第一行, 示例: 12.35, 即跳到 12.35% 位置")
+@click.option("-e", default="utf-8", help="文件编码, 默认为: utf-8, 示例: gbk")
+@click.option("-c", default=1, help="每次翻页是否清屏, 1清屏/0不清屏, 默认为: 1, 示例: 0")
+def main(p, r, s, e, c):
+    book_path = p
+    if book_path == "":
+        book_path = ReadLog.get_last_book()
+    # book_path = "D:/Downloads/仙逆-贴吧精校版.txt"
 
-# 每页显示的行数
-page_row = 1
+    # 每页显示的行数
+    page_row = r
 
-# 阅读进度
-schedule = ""
+    # 文件编码
+    encoding = e
 
-# 每次翻页是否清屏
-clear = False
+    # 每次翻页是否清屏, 1清屏/0不清屏
+    clear = c
+
+    # 阅读进度(当前行数/总行数)
+    schedule = s
+
+    lines = read_txt(book_path, encoding)
+    tip(book_path)
+    if schedule != 0:
+        pos = math.floor(schedule * len(lines))
+    else:
+        pos = ReadLog.read_schedule(book_path)
+    while True:
+        key = msvcrt.getch()
+        clear_screen(clear)
+        if is_quit_key(key):
+            break
+        if is_page_up_key(key):
+            pos = read_page(lines, pos - page_row * 2, page_row)
+        elif is_page_down_key(key):
+            pos = read_page(lines, pos, page_row)
+        elif is_clear_key(key):
+            os.system("cls")
+    ReadLog.write_schedule(book_path, pos - 1)
+    print("当前进度: %s%%" % str(round((pos - 1) / len(lines), 4) * 100))
 
 
 # 读一页
-def read_page(file: TextIO):
-    for i in range(0, page_row):
-        print(file.readline())
+def read_page(lines: list, pos: int, page_row: int):
+    if pos <= 0:
+        pos = 1
+    string = ""
+    ret = pos
+    for i in range(pos - 1, page_row + pos - 1):
+        if i >= len(lines):
+            string += "\r\n已到结尾"
+            break
+        ret += 1
+        string += lines[i]
+    print(string)
+    return ret
 
 
-def check_path():
-    return True
-
-
-def seek(file: TextIO, pos: int) -> int:
-    try:
-        file.seek(pos)
-        for i in range(0, 20):
-            file.readline()
-    except UnicodeDecodeError as error:
-        pos += 1
-        seek(file, pos)
-    else:
-        file.seek(pos)
-        return file.tell()
-
-
-def clear_screen():
-    if clear:
+def clear_screen(clear: int):
+    if clear == 1:
         os.system("cls")
 
 
-"""
-todo: 上翻一页
-
-"""
+def is_quit_key(key):
+    return key == b"q" or key == b"Q"
 
 
-# -row num: 每页显示的行数
-def main():
-    if not check_path():
-        return
-    tip()
-    file = open(book_path, mode="r", encoding="utf-8")
-    size = os.path.getsize(book_path)
-    if schedule != "":
-        pos = seek(file, int(schedule * size))
-    else:
-        pos = seek(file, ReadLog.get_schedule(book_path))
-    while True:
-        key = msvcrt.getch()
-        clear_screen()
-        if is_quit(key):
-            break
-        if is_page_up(key):
-            # file.seek(pos)
-            read_page(file)
-        elif is_page_down(key):
-            pos = file.tell()
-            read_page(file)
-    ReadLog.set_schedule(book_path, pos)
-    print(round(file.tell() / size, 2))
-
-
-def is_quit(key):
-    return key == b"q"
-
-
-def is_page_up(key):
+def is_page_up_key(key):
     """
     上方向键: H
     """
-    return key == b"H"
+    return key == b"w" or key == b"W"
 
 
-def is_page_down(key):
+def is_page_down_key(key):
     """
     下方向键: P
     """
-    return key == b"P"
+    return key == b"s" or key == b"S"
 
 
-def tip():
+def is_clear_key(key):
+    """
+    其他键清屏
+    """
+    return (not is_quit_key(key)) and (not is_page_up_key(key)) and (not is_page_down_key(key))
+
+
+def tip(book_path):
     """提示性输出"""
     print(os.path.basename(book_path))
     print("按下 '下方向键' 向下翻页")
@@ -108,6 +109,20 @@ def tip():
 
 def init_app_attribute():
     app.root = os.path.split(sys.argv[0])[0]
+
+
+def read_txt(path: str, encoding: str) -> list:
+    if not os.path.isfile(path):
+        print("文件不存在")
+        sys.exit(1)
+    try:
+        file = open(path, mode="r", encoding=encoding)
+        lines = file.readlines()
+        file.close()
+    except UnicodeDecodeError:
+        print("无法以%s编码打开文件, 请选择其他编码方式" % encoding)
+        sys.exit(1)
+    return lines
 
 
 if __name__ == "__main__":
